e02d2da2698e4e4354833e0c6e2789e8f75e3c7e
victorescande <victor.escande@elev.ga.ntig.se>
2023-08-31 13:28:51 +0200

    bbh

---
+++ intro.py
@@ -0,0 +1,23 @@
+#print(20+20)
+#print(20 - 10)
+#print(10 / 3)
+#print(10 // 3)
+#print(20*20)
+
+x = input("vad heter du: ")
+print("hej " + x)
+
+age = input("hur gammal är du?")
+
+print(x)
+print(age)
+
+print("hej", x, "du är", age, "år")
+
+print("""
+Hej folket!
+hur mår ni idag,
+hoppas ni mår bra!""")
+
+print("Resultat; \nTEINF torska")
+
---
+++ modul1.py
@@ -0,0 +1,38 @@
+print ("hello world")
+print('"hello world"')
+print("""
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+lorum ipsum
+
+""")
+
+
+print ("hej" "\nhej")
+
+print("hej" + " hej")
+
+
+
+x = input("Hej och välkommen vad heter du?: ")
+print("hej " + x,"!")
+lol = input("vad är din favoritfärg? ")
+
+age = input("hur gammal är du? ")
+
+print(x)
+print(age)
+
+print("\nhej", x, "stämmer det att du är", age, "år gammal, och att du gillar färgen", lol + "?"), 
+o = input(":")
+if o == "ja":
+    print("perfekt")
+elif o != "ja":
+    print ("fan också")
